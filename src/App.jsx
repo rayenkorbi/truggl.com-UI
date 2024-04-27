727a8fb22cf12af85d90dde3219f82037d27791a
Rayen <78444938+rayenkorbi@users.noreply.github.com>
2024-04-27 16:37:58 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import "./styles.css";
+import Spline from "@splinetool/react-spline";
+
+export default function App() {
+  return (
+    <Spline scene="https://prod.spline.design/47QCo9TZ1Pio6ik2/scene.splinecode" />
+  );
+}
